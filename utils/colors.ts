c16fe3c4c783a533ce5d43b02b148e7904d473c7
Siegfred Gamboa <siegfredgamboa@MacBook-Air-7.local>
2022-07-22 06:16:15 +0300

    created colors and layout guide comonents for the app

---
+++ utils/colors.ts
@@ -0,0 +1,22 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  yellow: '#FFD768',
+  lightYellow: '#FFE18D',
+  deepPink: '#D6365C',
+  white: '#FFFFFF',
+  offWhite: '#F6F6F6',
+  grayWhite: '#F5F5F5',
+  black: '#232323',
+  darkGray: '#DEDEDE',
+  gray: '#F2F2F2',
+  lightGray: '#FCFCFC',
+  lighterGray: '#F3F3F3',
+  success: '#00DC8D',
+  pink: '#EF4E74',
+  backgroundGray: '#8A979F',
+  darkBlue: '#3B5998',
+  royalBlue: '#4169E1',
+  lightGreen: '#A3E172',
+  mediumBlue: '#0000CD',
+  navy: '#000080',
+}
---
+++ utils/layout.ts
@@ -0,0 +1,9 @@
+export const breakpoints = {
+  xs: '375px',
+  sm: '480px',
+  md: '640px',
+  lg: '1024px',
+  xl: '1200px',
+  xxl: '1440px',
+  xxxl: '1920px',
+}
