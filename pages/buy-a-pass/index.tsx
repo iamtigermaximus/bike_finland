bd24921aa806a04c14cbb4c49a01557b80a68d42
Siegfred Gamboa <siegfredgamboa@MacBook-Air-7.local>
2022-07-30 14:31:14 +0300

    refactor buy a pass page, added content on the page

--- pages/buy-a-pass/index.tsx
+++ pages/buy-a-pass/index.tsx
@@ -2,9 +2,11 @@ import Head from 'next/head'
 import styled from 'styled-components'
 import colors from '../../utils/colors'
 import { breakpoints as bp } from '../../utils/layout'
+import Image from 'next/image'
+import CityBike2 from '../../assets/images/City_Bike2.jpeg'
 
 const Container = styled.div`
-  background: ${colors.gray};
+  background: ${colors.white};
   height: 100vh;
 `
 
@@ -25,6 +27,130 @@ const Heading = styled.h1`
   }
 `
 
+const HeroContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 40px 10px;
+
+  @media (min-width: ${bp.md}) {
+    flex-direction: row;
+  }
+`
+
+const HeroTextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const HeroHeading = styled.h1`
+  font-weight: 400;
+  font-size: 30px;
+  padding: 20px;
+
+  @media (min-width: ${bp.md}) {
+    font-size: 25px;
+  }
+`
+
+const HeroTexts = styled.h1`
+  font-weight: 400;
+  font-size: 13px;
+  padding: 10px;
+
+  @media (min-width: ${bp.md}) {
+    font-size: 15px;
+  }
+
+  @media (min-width: ${bp.lg}) {
+    font-size: 25px;
+    padding: 10px 70px;
+  }
+`
+const ImageContainer = styled.div`
+  padding: 20px;
+`
+const BuyPassContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: ${colors.gray};
+  padding-bottom: 50px;
+`
+const CardsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+
+  @media (min-width: ${bp.md}) {
+    flex-direction: row;
+  }
+`
+const BuyPassCard = styled.div`
+  width: 100%;
+  height: 100%;
+  background: ${colors.white};
+  margin-bottom: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 20px;
+
+  @media (min-width: ${bp.md}) {
+    margin: 20px;
+  }
+
+  @media (min-width: ${bp.lg}) {
+    padding: 20px 50px;
+  }
+`
+
+const BuyPassCardTitle = styled.h1`
+  font-size: 30px;
+  font-weight: 500;
+  padding: 5px;
+
+  @media (min-width: ${bp.md}) {
+    margin: 10px;
+  }
+`
+
+const BuyPassCardSubtexts = styled.h1`
+  font-size: 13px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+
+  @media (min-width: ${bp.lg}) {
+    font-size: 16px;
+  }
+`
+
+const BuyPassCardPrice = styled.h1`
+  font-size: 50px;
+  font-weight: 500;
+  padding: 0 5px;
+
+  @media (min-width: ${bp.md}) {
+    margin: 10px;
+  }
+`
+
+const BuyPassCardSelect = styled.button`
+background: ${colors.yellow};
+padding: 10px;
+width:100%;
+margin: 10px;
+border: 1px solid ${colors.yellow};
+border-radius: 5px;
+  }
+`
+
 const BuyPass = () => {
   return (
     <Container>
@@ -36,6 +162,51 @@ const BuyPass = () => {
       <PageHeadingContainer>
         <Heading>Buy a Pass</Heading>
       </PageHeadingContainer>
+      <HeroContainer>
+        <HeroTextContainer>
+          <HeroHeading>Helsinki and Espoo</HeroHeading>
+          <HeroTexts>
+            Find the closest available bike and hop on! The bikes are available
+            from the beginning of April until the end of October.
+          </HeroTexts>
+        </HeroTextContainer>
+        <ImageContainer>
+          <Image src={CityBike2} alt='City Bike' width={900} height={500} />
+        </ImageContainer>
+      </HeroContainer>
+      <BuyPassContainer>
+        <HeroHeading>Buy a Pass </HeroHeading>
+        <CardsContainer>
+          <BuyPassCard>
+            <BuyPassCardTitle>Season</BuyPassCardTitle>
+            <BuyPassCardSubtexts>
+              Until 31 October 2022, unlimited number of up to 30-minute rides,
+              1 bike
+            </BuyPassCardSubtexts>
+            <BuyPassCardPrice>€35</BuyPassCardPrice>
+            <BuyPassCardSubtexts>+ potential extra charges</BuyPassCardSubtexts>
+            <BuyPassCardSelect>Select</BuyPassCardSelect>
+          </BuyPassCard>
+          <BuyPassCard>
+            <BuyPassCardTitle>Week</BuyPassCardTitle>
+            <BuyPassCardSubtexts>
+              Today + 6 days, unlimited number of up to 30-minute rides, 1 bike
+            </BuyPassCardSubtexts>
+            <BuyPassCardPrice>€10</BuyPassCardPrice>
+            <BuyPassCardSubtexts>+ potential extra charges</BuyPassCardSubtexts>
+            <BuyPassCardSelect>Select</BuyPassCardSelect>
+          </BuyPassCard>
+          <BuyPassCard>
+            <BuyPassCardTitle>Day</BuyPassCardTitle>
+            <BuyPassCardSubtexts>
+              24h, unlimited number of up to 30-minute rides, 1-4 bikes, €5/bike
+            </BuyPassCardSubtexts>
+            <BuyPassCardPrice>€5</BuyPassCardPrice>
+            <BuyPassCardSubtexts>+ potential extra charges</BuyPassCardSubtexts>
+            <BuyPassCardSelect>Select</BuyPassCardSelect>
+          </BuyPassCard>
+        </CardsContainer>
+      </BuyPassContainer>
     </Container>
   )
 }
