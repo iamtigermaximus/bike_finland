3769d65370b62f57102c4c42c66264e7788f61ba
Siegfred Gamboa <siegfredgamboa@MacBook-Air-7.local>
2022-08-07 04:35:15 +0300

    refactor home page, removed unused codes

--- pages/index.tsx
+++ pages/index.tsx
@@ -84,66 +84,7 @@ const CardsContainer = styled.div`
     flex-direction: row;
   }
 `
-const BuyPassCard = styled.div`
-  width: 100%;
-  height: 100%;
-  background: ${colors.white};
-  margin-bottom: 10px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  padding: 20px;
 
-  @media (min-width: ${bp.md}) {
-    margin: 20px;
-  }
-
-  @media (min-width: ${bp.lg}) {
-    padding: 20px 50px;
-  }
-`
-
-const BuyPassCardTitle = styled.h1`
-  font-size: 30px;
-  font-weight: 500;
-  padding: 5px;
-
-  @media (min-width: ${bp.md}) {
-    margin: 10px;
-  }
-`
-
-const BuyPassCardSubtexts = styled.h1`
-  font-size: 13px;
-  width: 100%;
-  display: flex;
-  justify-content: center;
-
-  @media (min-width: ${bp.lg}) {
-    font-size: 16px;
-  }
-`
-
-const BuyPassCardPrice = styled.h1`
-  font-size: 50px;
-  font-weight: 500;
-  padding: 0 5px;
-
-  @media (min-width: ${bp.md}) {
-    margin: 10px;
-  }
-`
-
-const BuyPassCardSelect = styled.button`
-background: ${colors.yellow};
-padding: 10px;
-width:100%;
-margin: 10px;
-border: 1px solid ${colors.yellow};
-border-radius: 5px;
-  }
-`
 const Home: NextPage = () => {
   return (
     <Container>
