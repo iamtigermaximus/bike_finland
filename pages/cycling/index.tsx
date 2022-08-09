4f415863af3fda019467cdae2a655c53591bfcc4
Siegfred Gamboa <siegfredgamboa@MacBook-Air-7.local>
2022-08-09 05:47:48 +0300

    refactor map, changed the bike stations icon and added and added button to pan to center when clicked

--- pages/cycling/index.tsx
+++ pages/cycling/index.tsx
@@ -7,6 +7,7 @@ import colors from '../../utils/colors'
 import { breakpoints as bp } from '../../utils/layout'
 import { connectToDatabase } from '../../lib/mongodb'
 import { useJsApiLoader, GoogleMap, MarkerF } from '@react-google-maps/api'
+import { FaLocationArrow } from 'react-icons/fa'
 
 export type CardProps = {
   id: number
@@ -108,10 +109,16 @@ const LoadMoreButton = styled.button`
   border-radius: 5px;
   border: 1px solid ${colors.yellow};
 `
+const PanToCenterButton = styled.button`
+  padding: 10px 5px;
+  border: none;
+  font-size: 25px;
+  color: orange;
+`
 
 const Cycling: React.FC<CardProps> = ({ stations }: any) => {
   const [searchStation, setSearchStation] = useState('')
-  const [map, setMap] = useState(null)
+  const [map, setMap] = useState<any | null>(/** @type google.maps.Map */ null)
   const [noOfStations, setNoOfStations] = useState(10)
   const [location, setLocation] = useState({ lat: 60.1704, lng: 24.9522 })
 
@@ -152,22 +159,13 @@ const Cycling: React.FC<CardProps> = ({ stations }: any) => {
     height: '100%',
   }
 
-  const blueDot = {
-    fillColor: 'blue',
-    fillOpacity: 1,
-    path: google.maps.SymbolPath.CIRCLE,
-    scale: 8,
-    strokeColor: 'white',
-    strokeWeight: 2,
-  }
-
-  const stationDot = {
+  const stationIcon = {
     fillColor: 'orange',
     fillOpacity: 1,
-    path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
-    scale: 5,
-    strokeColor: 'white',
-    strokeWeight: 2,
+    path: 'M12 23c-6.071 0-11-4.929-11-11s4.929-11 11-11 11 4.929 11 11-4.929 11-11 11zm0-23c-6.623 0-12 5.377-12 12s5.377 12 12 12 12-5.377 12-12-5.377-12-12-12zm-1.379 12.996c-.125-1.063-.704-2.004-1.599-2.601l.488-.962 1.639 3.563h-.528zm-3.621 3.215c-1.534 0-2.782-1.248-2.782-2.782s1.248-2.783 2.782-2.783c.292 0 .581.046.857.135l-.944 1.863c-.397.043-.703.38-.703.785 0 .435.355.789.79.789.266 0 .515-.136.661-.357h2.088c-.21 1.346-1.377 2.35-2.749 2.35zm.704-3.215l.924-1.824c.602.437 1.006 1.094 1.121 1.824h-2.045zm6.559-3.778l-2.426 3.206-1.478-3.206h3.904zm2.737 6.993c-1.534 0-2.782-1.248-2.782-2.782 0-.969.509-1.871 1.334-2.376l.797 1.93c-.09.132-.138.285-.138.446 0 .435.354.789.789.789.435 0 .789-.354.789-.789 0-.38-.272-.706-.641-.776l-.796-1.93c.21-.051.428-.077.648-.077 1.534 0 2.782 1.249 2.782 2.783 0 1.534-1.248 2.782-2.782 2.782zm0-6.429c-.333 0-.663.045-.98.134l-1.232-2.991h-1.717c-.242 0-.432.2-.432.432 0 .238.194.433.432.433h1.133l.237.564h-4.469l-.266-.564h.145c.243 0 .432-.198.432-.433 0-.239-.193-.432-.432-.432h-1.78c-.115 0-.224.045-.305.126-.082.082-.127.191-.127.306 0 .238.194.433.432.433h.683l.294.641-.797 1.572c-.399-.147-.82-.221-1.251-.221-2.011 0-3.646 1.636-3.646 3.647 0 2.01 1.635 3.646 3.646 3.646 1.86 0 3.405-1.377 3.621-3.214h1.216l3.15-4.174.232.56c-1.153.647-1.865 1.859-1.865 3.182 0 2.01 1.635 3.646 3.646 3.646s3.646-1.636 3.646-3.646c0-2.011-1.635-3.647-3.646-3.647z',
+    scale: 1,
+    strokeColor: 'orange',
+    strokeWeight: 1,
   }
 
   return (
@@ -186,8 +184,9 @@ const Cycling: React.FC<CardProps> = ({ stations }: any) => {
             zoom={15}
             center={location}
             mapContainerStyle={containerStyle}
+            onLoad={(map) => setMap(map)}
           >
-            <MarkerF position={location} icon={blueDot} />
+            <MarkerF position={location} />
             {stations.map((station: any) => {
               return (
                 <MarkerF
@@ -195,7 +194,7 @@ const Cycling: React.FC<CardProps> = ({ stations }: any) => {
                   position={{ lat: station.y, lng: station.x }}
                   title={station.Osoite}
                   label={{ text: `${station.Nimi}`, color: 'white' }}
-                  icon={stationDot}
+                  icon={stationIcon}
                 />
               )
             })}
@@ -208,6 +207,9 @@ const Cycling: React.FC<CardProps> = ({ stations }: any) => {
               value={searchStation}
               onChange={(e) => setSearchStation(e.target.value)}
             />
+            <PanToCenterButton onClick={() => map.panTo(location)}>
+              <FaLocationArrow />
+            </PanToCenterButton>
           </SearchInputContainer>
           {stations
             .filter((station: any) => {
