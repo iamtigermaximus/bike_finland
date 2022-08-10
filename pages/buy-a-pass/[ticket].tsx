31e1e8fad4d86ead1aa5b62c34f966cb6d37cb51
Siegfred Gamboa <siegfredgamboa@MacBook-Air-7.local>
2022-08-10 03:43:49 +0300

    refactor ticket page, added contents agreements checkbox and continue button

--- pages/buy-a-pass/[ticket].tsx
+++ pages/buy-a-pass/[ticket].tsx
@@ -5,6 +5,7 @@ import colors from '../../utils/colors'
 import Head from 'next/head'
 import Image from 'next/image'
 import CityBike3 from '../../assets/images/City_Bike3.png'
+import { breakpoints as bp } from '../../utils/layout'
 
 const Container = styled.div`
   background: ${colors.gray};
@@ -48,6 +49,101 @@ const PassCardContentContainer = styled.div`
   width: 100%;
   height: 100vh;
 `
+const SelectedPassCardInfo = styled.div`
+  padding: 10px 30px;
+  display: flex;
+  flex-direction: column;
+  background: ${colors.gray};
+  width: 100%;
+  border: 1px solid ${colors.darkGray};
+  border-radius: 5px;
+`
+
+const PassTypeContainer = styled.div`
+  background: ${colors.gray};
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  @media (min-width: ${bp.sm}) {
+    flex-direction: row;
+  }
+`
+
+const CityTitle = styled.h1`
+  font-size: 16px;
+  padding: 5px;
+`
+const PassType = styled.h1`
+  font-size: 20px;
+  font-weight: 800;
+  padding: 5px;
+
+  @media (min-width: ${bp.sm}) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`
+const PassTypeInfo = styled.h1`
+  font-size: 13px;
+  padding: 5px;
+
+  @media (min-width: ${bp.sm}) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`
+const PassTypePrice = styled.h1`
+  font-size: 30px;
+  font-weight: 800;
+  padding: 5px;
+`
+
+const AgreementsContainer = styled.div`
+  background: ${colors.darkGray};
+  display: flex;
+  flex-direction: column;
+  margin: 20px 0;
+  padding: 20px 0;
+`
+const CheckboxContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin: 10px;
+`
+const Checkbox = styled.input`
+  background: ${colors.deepPink};
+  margin: 0 10px;
+`
+const Label = styled.label`
+  padding: 0 10px;
+`
+
+const AgreementsButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin: 10px 0;
+`
+const ContinueButton = styled.button`
+  padding: 5px 10px;
+  margin: 0 10px;
+  width: 100px;
+  border-radius: 5px;
+  border: none;
+  background: ${colors.yellow};
+`
+const AbortButton = styled.button`
+  padding: 5px 10px;
+  margin: 0 10px;
+  width: 100px;
+  border-radius: 5px;
+  border: none;
+  background: ${colors.yellow};
+`
 
 const Ticket = () => {
   const router = useRouter()
@@ -67,18 +163,55 @@ const Ticket = () => {
       <PassCardContainer>
         <Image src={CityBike3} alt='City Bike' width={40} height={30} />
         <PassCardTitleContainer>
+          {<PassCardTitle>{`Buy a  ${ticket} pass`}</PassCardTitle>}
+        </PassCardTitleContainer>
+      </PassCardContainer>
+      <PassCardContentContainer>
+        <SelectedPassCardInfo>
+          <CityTitle>Helsinki and Espoo</CityTitle>
           {ticket === 'season' ? (
-            <PassCardTitle>Buy a Season Pass</PassCardTitle>
+            <PassTypeContainer>
+              <PassType>Season Ticket</PassType>
+              <PassTypeInfo>until 31.10.2022</PassTypeInfo>
+              <PassTypePrice>35 €</PassTypePrice>
+            </PassTypeContainer>
           ) : ticket === 'weekly' ? (
-            <PassCardTitle>Buy a Weekly Pass</PassCardTitle>
+            <PassTypeContainer>
+              <PassType>Weekly Ticket</PassType>
+              <PassTypeInfo>Today + 6 days</PassTypeInfo>
+              <PassTypePrice>10 €</PassTypePrice>
+            </PassTypeContainer>
           ) : ticket === 'day' ? (
-            <PassCardTitle>Buy a Day Pass</PassCardTitle>
+            <PassTypeContainer>
+              <PassType>Day Ticket</PassType>
+              <PassTypeInfo>24h</PassTypeInfo>
+              <PassTypePrice>5 €</PassTypePrice>
+            </PassTypeContainer>
           ) : (
             ''
           )}
-        </PassCardTitleContainer>
-      </PassCardContainer>
-      <PassCardContentContainer></PassCardContentContainer>
+        </SelectedPassCardInfo>
+        <AgreementsContainer>
+          <CheckboxContainer>
+            <Checkbox type='checkbox' />
+            <Label>
+              I can use city bikes continuously for 30 minutes. After that, €1
+              will be charged for each additional 30 minutes.
+            </Label>
+          </CheckboxContainer>
+          <CheckboxContainer>
+            <Checkbox type='checkbox' />
+            <Label>
+              The maximum time of use in 5 hours. If I exceed this time, I will
+              be charged
+            </Label>
+          </CheckboxContainer>
+          <AgreementsButtonContainer>
+            <ContinueButton>Continue</ContinueButton>
+            <AbortButton> Abort</AbortButton>
+          </AgreementsButtonContainer>
+        </AgreementsContainer>
+      </PassCardContentContainer>
     </Container>
   )
 }
