58ae3ce22eb17e767d79aeaf7d44b1d1c22510f0
Siegfred Gamboa <siegfredgamboa@MacBook-Air-7.local>
2022-08-24 02:16:55 +0300

    refactor instruction card, removed chevron/ angle bracket

--- components/InstructionCard.tsx
+++ components/InstructionCard.tsx
@@ -46,34 +46,33 @@ const MoreInfoLink = styled.h1`
   }
 `
 
-
 const InstructionCard = () => {
   return (
     <>
-       <InstructionStepCard>
-  <InstructionCardTitle>REGISTER</InstructionCardTitle>
-  <Link href='/instructions'>
-    <MoreInfoLink>More ></MoreInfoLink>
-  </Link>
-</InstructionStepCard>
-<InstructionStepCard>
-  <InstructionCardTitle>PICK UP</InstructionCardTitle>
-  <Link href='/instructions'>
-    <MoreInfoLink>More ></MoreInfoLink>
-  </Link>
-</InstructionStepCard>
-<InstructionStepCard>
-  <InstructionCardTitle>RIDE</InstructionCardTitle>
-  <Link href='/instructions'>
-    <MoreInfoLink>More ></MoreInfoLink>
-  </Link>
-</InstructionStepCard>
-<InstructionStepCard>
-  <InstructionCardTitle>RETURN</InstructionCardTitle>
-  <Link href='/instructions'>
-    <MoreInfoLink>More > </MoreInfoLink>
-  </Link>
-</InstructionStepCard> 
+      <InstructionStepCard>
+        <InstructionCardTitle>REGISTER</InstructionCardTitle>
+        <Link href='/instructions'>
+          <MoreInfoLink>More...</MoreInfoLink>
+        </Link>
+      </InstructionStepCard>
+      <InstructionStepCard>
+        <InstructionCardTitle>PICK UP</InstructionCardTitle>
+        <Link href='/instructions'>
+          <MoreInfoLink>More...</MoreInfoLink>
+        </Link>
+      </InstructionStepCard>
+      <InstructionStepCard>
+        <InstructionCardTitle>RIDE</InstructionCardTitle>
+        <Link href='/instructions'>
+          <MoreInfoLink>More...</MoreInfoLink>
+        </Link>
+      </InstructionStepCard>
+      <InstructionStepCard>
+        <InstructionCardTitle>RETURN</InstructionCardTitle>
+        <Link href='/instructions'>
+          <MoreInfoLink>More... </MoreInfoLink>
+        </Link>
+      </InstructionStepCard>
     </>
   )
 }
