d3962394ff56784c63ffc96fb7ea1f53bf54e0cb
Siegfred Gamboa <siegy.gamboa@gmail.com>
2022-11-02 02:58:36 +0200

    refactor single station component, separate css files

--- pages/cycling/[name].tsx
+++ pages/cycling/[name].tsx
@@ -1,128 +1,25 @@
 import { useRouter } from 'next/router'
-import styled from 'styled-components'
 import Head from 'next/head'
-import colors from '../../utils/colors'
-import { breakpoints as bp } from '../../utils/layout'
 import { useJsApiLoader, GoogleMap, MarkerF } from '@react-google-maps/api'
 import { useEffect, useState } from 'react'
-
-const Container = styled.div`
-  background: ${colors.gray};
-  height: 100%;
-`
-const PageHeadingContainer = styled.div`
-  background: ${colors.darkGray};
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 30px 0;
-`
-const Heading = styled.h1`
-  text-shadow: 0.6px 0 0;
-`
-
-const StationLocationContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
-  @media (min-width: ${bp.lg}) {
-    flex-direction: row;
-    align-items: flex-start;
-  }
-`
-const BikeStationsContainer = styled.div`
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  order: 2;
-  margin: 10px;
-  padding: 10px;
-  width: 100%;
-  border: 1px solid ${colors.darkGray};
-
-  @media (min-width: ${bp.lg}) {
-    order: 1;
-    width: 40%;
-  }
-`
-const MapContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  height: 50vh;
-  width: 100%;
-  margin: 10px;
-  order: 1;
-
-  @media (min-width: ${bp.lg}) {
-    order: 2;
-    width: 60%;
-    height: 100vh;
-  }
-`
-const SingleStationContainer = styled.div`
-  padding: 10px 30px;
-  display: flex;
-  flex-direction: column;
-  background: ${colors.yellow};
-`
-const SingleStationTitle = styled.h1`
-  font-size: 25px;
-  font-weight: 500;
-  padding-bottom: 10px;
-`
-const SingleStationName = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  padding-bottom: 10px;
-`
-const SingleStationNumber = styled.h1`
-  font-size: 20px;
-  font-weight: 500;
-  padding-bottom: 5px;
-`
-const SingleStationAddress = styled.h1`
-  font-size: 20px;
-  font-weight: 500;
-  padding-bottom: 5px;
-`
-
-const SingleStationCity = styled.h1`
-  font-size: 20px;
-  font-weight: 500;
-  padding-bottom: 5px;
-`
-
-const SingleStationCapacity = styled.h1`
-  font-size: 20px;
-  font-weight: 500;
-  padding-bottom: 5px;
-`
-
-const BackContainer = styled.div`
-  margin: 30px;
-  color: ${colors.navy};
-  width: 70px;
-  cursor: pointer;
-`
-
-const InputContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  padding: 10px;
-  width: 100%;
-`
-
-const Input = styled.input`
-  padding: 10px 15px;
-  width: 100%;
-  border-radius: 5px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`
+import {
+  BackContainer,
+  BikeStationsContainer,
+  Container,
+  Heading,
+  Input,
+  InputContainer,
+  MapContainer,
+  PageHeadingContainer,
+  SingleStationAddress,
+  SingleStationCapacity,
+  SingleStationCity,
+  SingleStationContainer,
+  SingleStationName,
+  SingleStationNumber,
+  SingleStationTitle,
+  StationLocationContainer,
+} from './cycling-station.styles'
 
 const BikeStation = () => {
   const router = useRouter()
