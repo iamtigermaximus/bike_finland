bd74e113990ca4dfa0b2a20d437c0520fd93b015
Siegfred Gamboa <siegy.gamboa@gmail.com>
2022-11-02 02:40:57 +0200

    refactor buy-a-pass component, separate css files

--- pages/buy-a-pass/index.tsx
+++ pages/buy-a-pass/index.tsx
@@ -1,87 +1,18 @@
 import Head from 'next/head'
-import styled from 'styled-components'
-import colors from '../../utils/colors'
-import { breakpoints as bp } from '../../utils/layout'
+import {
+  BuyPassContainer,
+  Container,
+  Heading,
+  HeroContainer,
+  HeroHeading,
+  HeroTextContainer,
+  HeroTexts,
+  ImageContainer,
+  PageHeadingContainer,
+} from './buy-a-pass.styles'
 import Image from 'next/image'
 import CityBike2 from '../../assets/images/City_Bike2.jpeg'
 import PassCard from '../../components/passcard/PassCard'
-
-const Container = styled.div`
-  background: ${colors.white};
-  height: 100vh;
-`
-
-const PageHeadingContainer = styled.div`
-  background: ${colors.darkGray};
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 30px 0;
-`
-
-const Heading = styled.h1`
-  text-shadow: 0.6px 0 0;
-  font-size: 20px;
-
-  @media (min-width: ${bp.md}) {
-    font-size: 40px;
-  }
-`
-
-const HeroContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin: 40px 10px;
-
-  @media (min-width: ${bp.md}) {
-    flex-direction: row;
-  }
-`
-
-const HeroTextContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`
-
-const HeroHeading = styled.h1`
-  font-weight: 400;
-  font-size: 30px;
-  padding: 20px;
-
-  @media (min-width: ${bp.md}) {
-    font-size: 25px;
-  }
-`
-
-const HeroTexts = styled.h1`
-  font-weight: 400;
-  font-size: 13px;
-  padding: 10px;
-
-  @media (min-width: ${bp.md}) {
-    font-size: 15px;
-  }
-
-  @media (min-width: ${bp.lg}) {
-    font-size: 25px;
-    padding: 10px 70px;
-  }
-`
-const ImageContainer = styled.div`
-  padding: 20px;
-`
-const BuyPassContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  background: ${colors.gray};
-  padding-bottom: 50px;
-`
 
 const BuyPass = () => {
   return (
